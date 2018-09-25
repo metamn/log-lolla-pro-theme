571053aae3bcdbaddf4ee2a9e586ccf4cbf75767
metamn <bartus.csongor@gmail.com>
2018-09-25 09:25:12 +0300

    Child theme ready for customization

--- assets/js/log-lolla-pro-theme.js
+++ assets/js/log-lolla-pro-theme.js
@@ -1,5 +1,5 @@
 /**
- * Javascript functions for the Log Lolla Pro Theme Theme
+ * Javascript functions for the Log Lolla Pro Theme
  */
 
 
@@ -25,63 +25,10 @@ var archiveCounterClick = function() {
     }
 }
 
-// Click on post content
-var postContentClick = function() {
-  var postContent = document.querySelectorAll('.post-with-sidebar .post-content-between-sidebars');
-
-  function postContentRemoveClicks() {
-    for (var i = 0; i < postContent.length; i++) {
-      postContent[i].parentNode.classList.remove('post-with-sidebar--clicked');
-    }
-  }
-
-  function onMouseClick(index, event) {
-    var post = postContent[index].parentNode;
-
-    if (post.classList.contains('post-with-sidebar--clicked')) {
-      post.classList.remove('post-with-sidebar--clicked')
-    } else {
-      postContentRemoveClicks();
-      post.classList.add('post-with-sidebar--clicked');
-    }
-
-    event.stopPropagation();
-  }
-
-  for (var i = 0; i < postContent.length; i++) {
-    postContent[i].addEventListener('click', onMouseClick.bind(null, i), false);
-  }
-}
-
-// Click on the hamburger menu
-var menuHamburgerClick = function(ID) {
-  var menuHamburger = document.querySelector('.header-menu-hamburger');
-  var menuMain = document.querySelector('.header-menu');
-
-  function onViewChange(event) {
-    menuMain.classList.toggle('header-menu--closed');
-    menuHamburger.classList.toggle('header-menu-hamburger--closed');
-    event.stopPropagation();
-  }
-
-  menuHamburger.addEventListener('click', onViewChange, false);
-}
-
-
 // Run functions once the document is ready
 document.addEventListener('DOMContentLoaded', function(){
 	// Archive counter click
 	if (document.querySelector('.archive-counter-list')) {
 		archiveCounterClick();
-	}
-
-	// Post content click
-	if (document.querySelector('.post-list')) {
-		postContentClick();
-	}
-
-	// Hamburger menu
-	if (document.querySelector('.header-menu-hamburger')) {
-		menuHamburgerClick('.header-menu-hamburger');
 	}
 }, false);
--- functions.php
+++ functions.php
@@ -16,11 +16,23 @@ if ( ! function_exists( 'log_lolla_pro_theme_scripts' ) ) :
 	 * @return void
 	 */
 	function log_lolla_pro_theme_scripts() {
+		// Parent style will be removed.
+		wp_dequeue_style('log-lolla-pro-theme-style');
+
+		// It seems style is added by deafult, we don't need this here ...
+		/*
 		$timestamp = filemtime( get_template_directory() . '/style.css' );
 		wp_enqueue_style( 'log-lolla-pro-theme-style', get_stylesheet_uri(), array(), $timestamp );
+		*/
 
+		// This script depends on the parent
 		$timestamp = filemtime( get_template_directory() . '/assets/js/log-lolla-pro-theme.js' );
-		wp_enqueue_script( 'log-lolla-pro-theme', get_theme_file_uri( '/assets/js/log-lolla-pro-theme.js' ), array(), $timestamp );
+		wp_enqueue_script(
+			'log-lolla-pro-theme-script',
+			get_theme_file_uri( '/assets/js/log-lolla-pro-theme.js' ),
+			array('log-lolla-theme'),
+			$timestamp
+		);
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'log_lolla_pro_theme_scripts' );
